186a41320df14addeeebbe400d3c6c483ffe43ae
Stacey Van Wyk <svanwyk722@gmail.com>
2026-04-01 13:24:00 +0200

    My first program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello " + name);
+
+        Console.WriteLine("Enter your age:");
+        int age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Next year you will be " + (age + 1));
+    }
+}
